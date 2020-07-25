bc91fbfcab240b38fa3b1926582c37d9e5132f40
Brendan Stromberger <brendan@wearethorn.org>
2020-07-25 16:25:54 -0700

    tmp

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import d3, { select, range, scaleLinear } from "d3";
 import Fraction from "fraction.js";
 import { Machine } from "xstate";
@@ -12,9 +12,10 @@ enum Condition {
 export const Board: React.FC<{ containerRect: DOMRect }> = ({
   containerRect,
 }) => {
+  const [activeCell, setActiveCell] = useState<number | null>(null);
   const boardRef = React.useRef<SVGSVGElement>(null);
-  const boardWidth = useDebounce(containerRect.width, 1000);
-  const boardHeight = useDebounce(containerRect.height, 1000);
+  const boardWidth = containerRect.width;
+  const boardHeight = containerRect.height;
   const columns = Math.floor((boardWidth / boardHeight) * 10);
   const rows = Math.floor((boardWidth / boardHeight) * 10);
 
@@ -23,8 +24,48 @@ export const Board: React.FC<{ containerRect: DOMRect }> = ({
   const gameState = React.useMemo(() => {
     return Array(columns * rows)
       .fill(null)
-      .map(() => (Math.random() >= 0.5 ? Condition.Alive : Condition.Dead));
+      .map(() => Condition.Dead);
   }, [columns, rows]);
+
+  const t = (idx: number) => idx - columns;
+  const tl = (idx: number) => t(idx) - 1;
+  const tr = (idx: number) => t(idx) + 1;
+  const l = (idx: number) => idx - 1;
+  const r = (idx: number) => idx + 1;
+  const b = (idx: number) => idx + columns;
+  const bl = (idx: number) => b(idx) - 1;
+  const br = (idx: number) => b(idx) + 1;
+
+  const getFill = React.useCallback(
+    (condition: Condition, idx: number) => {
+      console.log("activeCell", activeCell);
+      if (idx === activeCell) {
+        return "white";
+      } else if (
+        activeCell != null &&
+        [t, tl, tr, l, r, b, bl, br].some((fn) => fn(activeCell) === idx)
+      ) {
+        return "green";
+      } else {
+        return condition === Condition.Alive ? "black" : "red";
+      }
+    },
+    [activeCell]
+  );
+
+  const getXOffset = React.useCallback(
+    (_, i: number) => {
+      return (i % columns) * cellWidth;
+    },
+    [cellWidth, columns]
+  );
+
+  const getYOffset = React.useCallback(
+    (_, i: number) => {
+      return Math.floor(i / rows) * cellHeight;
+    },
+    [cellHeight, rows]
+  );
 
   React.useEffect(() => {
     if (boardRef.current) {
@@ -41,35 +82,33 @@ export const Board: React.FC<{ containerRect: DOMRect }> = ({
               .append("rect")
               .attr("width", cellWidth)
               .attr("height", cellHeight)
-              .attr("fill", (d) => (d === Condition.Alive ? "green" : "yellow"))
-              .attr("x", (_, i) => (i % columns) * cellWidth)
-              .attr("y", (_, i) => Math.floor(i / rows) * cellHeight),
+              .attr("fill", getFill)
+              .attr("x", getXOffset)
+              .attr("y", getYOffset)
+              .on("click", (_, idx) => setActiveCell(idx)),
           (update) =>
             update
               .attr("width", cellWidth)
               .attr("height", cellHeight)
-              .attr("fill", (d) => (d === Condition.Alive ? "green" : "yellow"))
-              .attr("x", (_, i) => (i % columns) * cellWidth)
-              .attr("y", (_, i) => Math.floor(i / rows) * cellHeight),
-
+              .attr("fill", getFill)
+              .attr("x", getXOffset)
+              .attr("y", getYOffset)
+              .on("click", (_, idx) => setActiveCell(idx)),
           (exit) => {
-            console.log("fart!");
             exit.remove();
           }
         );
     }
   }, [
-    gameState,
     cellHeight,
     cellWidth,
-    rows,
-    columns,
-    containerRect.width,
     containerRect.height,
+    containerRect.width,
+    gameState,
+    getFill,
+    getXOffset,
+    getYOffset,
   ]);
-
-  console.log("rects", document.querySelectorAll("rect").length);
-  console.log("gameState", gameState.length);
 
   return <svg ref={boardRef} />;
 };
